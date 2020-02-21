7c47e9c04284686915365eda5238d63c8be8b4f6
mareks-sinica <56246495+mareks-sinica@users.noreply.github.com>
2020-02-21 08:42:06 +0200

    Add files via upload

--- Fleskintro/main.py
+++ Fleskintro/main.py
@@ -1,19 +1,25 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
 
-app= Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return "<a href='/about'>Hi!</a>"
+  return "<a href ='/home'>Hey!</a>"
+
+@app.route('/home')
+def home():
+  return render_template('home.html')
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+  return render_template('about.html', active_page = 'about')
 
 @app.route('/contact')
 def contact():
-  return render_template('contact.html', phone = 872131231)
+  return render_template('contact.html', phone = "1111111")
 
+@app.route('/params')
+def params():
+  return render_template('params.html', args = request.args.to_dict())
 if __name__ == '__main__':
-  app.run(host = '0.0.0.0', port = 5211, threaded = True, 
-  debug = True)
+  app.run(host = '0.0.0.0', port = 5211, threaded = True, debug = True)
